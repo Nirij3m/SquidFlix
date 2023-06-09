cf3df9fc8480098ed0060261bd007bf94887e0d3
dansunavioon <115085867+dansunavioon@users.noreply.github.com>
2023-06-09 09:44:38 +0200

    Correction couleur + footer

--- src/js/navbar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("navbar-menu").addEventListener("click", function() {
-    var navbar_center = document.querySelector("div.navbar-center");
-    var navbar_right = document.querySelector("div.navbar-right");
-    navbar_center.classList.toggle("hidden");
-    navbar_right.classList.toggle("hidden");
-});
