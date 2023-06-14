8f3c7a4a6c3d9c7e8b4adb474a9077bb7eec46c1
AmalizzyDHY <120025995+AmalizzyDHY@users.noreply.github.com>
2023-06-14 09:26:49 +0200

    debug

--- src/js/recherche.js
+++ src/js/recherche.js
@@ -294,10 +294,11 @@ function results(txt){
         if ( counter === 1 || counter === 0 ){ //Une des 2 premières ligne
             if ( counter === 0 && txt[i] != "\n"){ //lettre pour savoir où écrire c'est txt[i=0]
                 lettre += txt[i];
+                console.log("Lettre : " + lettre);
             }
             if ( counter === 1 && txt[i] != "\n"){ //Temps d'exe
                 exe += txt[i] ;
-                //console.log("exe : " + exe);
+                console.log("exe : " + exe);
             }
             if ( txt[i] == "\n"){//On change de ligne
                 counter += 1 ;
@@ -306,19 +307,19 @@ function results(txt){
         else {            //On commence à lire des lignes avec les films
             if ( count == 0 && txt[i] != "\n" && txt[i] != ";"){ //Nom du directeur
                 director += txt[i] ;
-                console.log("director : " + director);
+                //console.log("director : " + director);
             }
             if ( count == 1 && txt[i] != "\n" && txt[i] != ";"){ //Titre du film
                 titre += txt[i] ;
-                console.log("titre : " + titre);
+                //console.log("titre : " + titre);
             }
             if ( count == 2 && txt[i] != "\n" && txt[i] != ";"){ //durer du film
                 time += txt[i] ;
-                console.log("time : " + time);
+                //console.log("time : " + time);
             }
             if ( count == 3 && txt[i] != "\n" && txt[i] != ";"){ //genre du film
                 genre += txt[i] ;
-                console.log("genre : " + genre);
+                //console.log("genre : " + genre);
             }
             if ( txt[i] == ";" ){ //on change de champ
                 count = ( count + 1 ) % 4 ;
@@ -366,6 +367,9 @@ function results(txt){
                     document.getElementsByClassName("container1")[0].appendChild(newCard); //On ajoute la nouvelle card
                     //console.log("bloup");
 
+                    animateCards();
+
+                    console.log(exe)
                     document.getElementsByClassName("exe").innerHTML = exe ;
                 }
 
@@ -400,6 +404,8 @@ function results(txt){
 
                     document.getElementsByClassName("container2")[0].appendChild(newCard); //On ajoute la carte au container pour l'affihcer
                     //console.log("bloup");
+
+                    animateCards();
 
                 }
 
